400ff41de6e49d9b80e42ac625c4a8825f1afbbd
Joel <joel.v.lehtonen@gmail.com>
2025-03-05 21:43:19 +0200

    works, need to add date validation and different errors

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,8 +1,6 @@
 #include "BitcoinExchange.hpp"
 
-BitcoinExchange::BitcoinExchange() {
-	_firstLineChecked = false;
-};
+BitcoinExchange::BitcoinExchange() { };
 BitcoinExchange::~BitcoinExchange() { };
 BitcoinExchange::BitcoinExchange(const BitcoinExchange& copy) 
 : _data(copy._data) { };
@@ -17,39 +15,29 @@ const BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& copy) {
 void BitcoinExchange::exchange(const std::string& input) {
 	copyDatabase();
 	readInput(input);
-	//copyInput(input);
-	// auto it = _data.begin();
-	// std::cout << "database #0 = " << it->first << " | " << it->second << std::endl;
-	// it++;
-	// std::cout << "database #1 = " << it->first << " | " << it->second << std::endl;
-	// it++;
-	// std::cout << "database #2 = " << it->first << " | " << it->second << std::endl;
-	// it = _input.begin();
-	// std::cout << "input #0 = " << it->first << " | " << it->second << std::endl;
-	// it++;
-	// std::cout << "input #1 = " << it->first << " | " << it->second << std::endl;
-};
+}
 
 void BitcoinExchange::copyDatabase() {
-	std::ifstream dataStream("minidata.csv");
+	bool firstLine = false;
+
+	std::ifstream dataStream("data.csv");
 	if (dataStream.is_open() == false)
-		throw std::runtime_error("The database could not be opened");
+		throw std::runtime_error("Database could not be opened");
 	std::string line;
 	while (getline(dataStream, line))
 	{
-		if (_firstLineChecked == false)
+		if (firstLine == false)
 		{
+			firstLine = true;
 			if (line.find("date") != std::string::npos && line.find("exchange_rate") != std::string::npos)
-			{
-				_firstLineChecked = true;
 				continue ;
-			}
-			_firstLineChecked = true;
 		}
 		size_t delim = line.find(',');
 		if (delim == std::string::npos)
 			throw std::runtime_error("Database incorrectly formatted");
 		std::string keyString = line.substr(0, delim);
+		if (keyString.empty() == true)
+			break ;
 		int keyInt = convertStringtoInt(keyString);
 		std::string valueString = line.substr(delim + 1);
 		float valueFloat = stof(valueString);
@@ -62,19 +50,35 @@ void BitcoinExchange::copyDatabase() {
 //TODO REFACTOR THIS
 void BitcoinExchange::readInput(const std::string& input)
 {
+	bool firstLine = false;
+
 	std::ifstream inputStream(input);
 	if (inputStream.is_open() == false)
 		throw std::runtime_error("The input could not be opened");
 	std::string line;
 	while (getline(inputStream, line))
 	{
+		if (firstLine == false)
+		{
+			firstLine = true;
+			if (line.find("date") != std::string::npos && line.find("value") != std::string::npos)
+				continue ;
+		}
 		size_t delim = line.find('|');
 		if (delim == std::string::npos)
 		{
-			processInput(0, 0); //make specific error codes later
+			printError(); //make specific error codes later
 			continue ;
 		}
 		std::string keyString = line.substr(0, delim);
+		if (keyString.empty() == true)
+		{
+			printError();
+			continue ;
+		}
+		checkValidDate(keyString);
+		// tm dateCheck;
+		// inputStream >> std::get_time(&dateCheck, "%Y-%m-%d");
 		int keyInt = convertStringtoInt(keyString);
 		std::string valueString = line.substr(delim + 1);
 		try {	
@@ -82,11 +86,15 @@ void BitcoinExchange::readInput(const std::string& input)
 			processInput(keyInt, valueFloat);
 		}
 		catch (...) {
-			processInput(0, 0); //make specific error codes later
+			printError(); //make specific error codes later
 			continue ;
 		}
 	}
 	inputStream.close();
+};
+
+void BitcoinExchange::checkValidDate(std::string keyString) {
+	
 };
 
 int BitcoinExchange::convertStringtoInt(std::string& keyString)
@@ -102,19 +110,21 @@ int BitcoinExchange::convertStringtoInt(std::string& keyString)
 
 void BitcoinExchange::processInput(int key, float value)
 {
-	// std::map<int, float>::const_iterator inputIt = _input.begin();
 	std::map<int, float>::const_reverse_iterator dataIt = _data.crbegin();
-	// while (inputIt != _input.end())
-	// {
-		//TODO check for invalid date (use gettime), if so print error and continue to next line
-		while (dataIt != _data.crend() && key > dataIt->first)
-			dataIt++;
-		//TODO check for invalid format, if so print error
+	//TODO check for invalid date (use gettime), if so print error and continue to next line
+	while (dataIt != _data.crend() && key < dataIt->first)
+		dataIt++;
+	if (dataIt == _data.crend())
+		dataIt = _data.crbegin();
+	if (value < 0 )
+		printError(); //make specific error codes later
+	else if (value >= INT_MAX)
+		printError(); //make specific error codes later
+	else
+	{
 		printDate(key);
 		printEquation(value, dataIt);
-		// inputIt++;
-		dataIt = _data.crbegin();
-	// }
+	}
 }
 
 void BitcoinExchange::printDate(int key)
@@ -132,6 +142,9 @@ void BitcoinExchange::printEquation(float value, std::map<int,float>::const_reve
 {
 	float result = value * dataIt->second;
 	std::cout << " => " << value << " = " << result << "\n";
-	//std::cout << " => " << dataIt->second << " = " << result << "\n";
 	//check width of the float, so all decimals get printed
 }
+
+void BitcoinExchange::printError() {
+	std::cout << "Error: bad input\n";
+};
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -5,18 +5,21 @@
 #include <fstream>
 #include <exception>
 #include <cctype>
+#include <climits>
+// #include <ctime>
+#include <iomanip>
 #include <algorithm>
 
 class BitcoinExchange {
 	private:
 		std::map<int,float>	_data;
-		//std::map<int,float>	_input;
-		bool				_firstLineChecked;
 
 		void	copyDatabase();
 		void	readInput(const std::string& input);
+		void	checkValidDate(std::string keyString);
 		int		convertStringtoInt(std::string& string);
 		void	processInput(int key, float value);
+		void	printError();
 		void	printDate(int key);
 		void	printEquation(float value, std::map<int,float>::const_reverse_iterator dataIt);
 	
